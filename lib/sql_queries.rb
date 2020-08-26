2de54a561b6fcf689ae5e7630dfb6124e5ee903c
Christie Yoon <cleex1999@gmail.com>
2020-08-26 18:41:11 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -15,7 +15,7 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-  "SELECT projects.title, SUM(pledges.amount) FROM projects JOIN pledges on pledges.project_idWHERE projects.funding_goal = 'funding_goal'"
+  "SELECT projects.title, SUM(pledges.amount) FROM projects JOIN pledges on pledges.project_id GROUP BY projects.title HAVING projects.funding_goal = pledges.amount"
 end
 
 
